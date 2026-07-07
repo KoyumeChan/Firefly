0d15ef3cabea8a0867a7a51786a7721b38a1179c
KoyumeChan <1006696646@qq.com>
2026-07-08 06:49:57 +0800

    v0.4

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -75,7 +75,7 @@ export const siteConfig: SiteConfig = {
 			alt: "⚙️",
 		},
 		// 导航栏标题
-		title: "镜中奇械",
+		title: "「木偶」",
 		// 全宽导航栏，导航栏是否占满屏幕宽度
 		widthFull: false,
 		// 导航菜单对齐方式，left：左对齐，center：居中
@@ -100,7 +100,7 @@ export const siteConfig: SiteConfig = {
 		// 打赏页面开关
 		sponsor: false,
 		// 留言板页面开关，需要配置评论系统
-		guestbook: true,
+		guestbook: false,
 		// 番组计划页面开关，含追番、游戏、书籍和音乐
 		bangumi: false,
 		// 相册页面开关
